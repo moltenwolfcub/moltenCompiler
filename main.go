87d0a3f015d8f21d8f229dbca93973825521b939
MoltenWolfCub <oliver@tomlins.org.uk>
2023-11-01 13:51:38 +0000

    read file into go
    molten code is being loaded into go for further parsing

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	if len(os.Args) != 2 {
+		fmt.Println("Bad usage! Correct usage is:\n\"molten <main.mltn>\"")
+		return
+	}
+	fileName := os.Args[1]
+
+	file, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Cannot read the file")
+		return
+	}
+
+	program := string(file)
+
+	print(program)
 }
