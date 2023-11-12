c59193403280539cf63d711355e746886a63be27
MoltenWolfCub <oliver@tomlins.org.uk>
2023-11-12 15:29:09 +0000

    refactored parser
    extracted out the repeated
    `p.peek().HasValue() && p.peek().MustGetValue().tokenType == tokType`
    into a sepatate method for less repetition and more readability

--- parser.go
+++ parser.go
@@ -32,68 +32,40 @@ func (p *Parser) ParseProg() opt.Optional[NodeProg] {
 }
 
 func (p *Parser) ParseStmt() opt.Optional[NodeStmt] {
-	if p.peek().MustGetValue().tokenType == exit {
-		if !p.peek(1).HasValue() || p.peek(1).MustGetValue().tokenType != openRoundBracket {
-			panic(errors.New("expected '(' after 'exit'"))
-		}
-
-		p.consume()
-		p.consume()
+	if p.tryConsume(exit).HasValue() {
+		p.mustTryConsume(openRoundBracket, "expected '(' after 'exit'")
 
 		var node NodeStmtExit
 
 		if nodeExpr := p.ParseExpr(); nodeExpr.HasValue() {
-			node = NodeStmtExit{expr: nodeExpr.MustGetValue()}
+			node = NodeStmtExit{nodeExpr.MustGetValue()}
 
-		} else if p.peek().HasValue() && p.peek().MustGetValue().tokenType == closeRoundBracket {
-			node = NodeStmtExit{
-				expr: NodeExpr{NodeTerm{NodeTermIntLiteral{intLiteral: Token{
-					tokenType: intLiteral,
-					value:     opt.ToOptional("0"),
-				}}}},
-			}
+		} else if p.tryConsume(closeRoundBracket).HasValue() {
+			node = NodeStmtExit{NodeExpr{NodeTerm{NodeTermIntLiteral{Token{
+				tokenType: intLiteral,
+				value:     opt.ToOptional("0"),
+			}}}}}
 		} else {
 			panic(errors.New("invalid expression"))
 		}
 
-		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == closeRoundBracket {
-			p.consume()
-		} else {
-			panic(errors.New("missing ')'"))
-		}
+		p.mustTryConsume(closeRoundBracket, "missing ')'")
+		p.mustTryConsume(semiColon, "missing ';'")
 
-		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == semiColon {
-			p.consume()
-		} else {
-			panic(errors.New("missing ';'"))
-		}
+		return opt.ToOptional(NodeStmt{node})
+	} else if p.tryConsume(_var).HasValue() {
 
-		return opt.ToOptional(NodeStmt{variant: node})
-	} else if p.peek().MustGetValue().tokenType == _var {
-		p.consume()
+		tok := p.mustTryConsume(identifier, "expected variable name after `var`")
+		node := NodeStmtVarDeclare{tok}
 
-		var node NodeStmtVarDeclare
+		p.mustTryConsume(semiColon, "missing ';'")
 
-		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == identifier {
-			node = NodeStmtVarDeclare{ident: p.consume()}
-		} else {
-			panic(errors.New("expected variable name after `var`"))
-		}
-
-		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == semiColon {
-			p.consume()
-		} else {
-			panic(errors.New("missing ';'"))
-		}
-
-		return opt.ToOptional(NodeStmt{variant: node})
-	} else if p.peek().MustGetValue().tokenType == identifier &&
-		p.peek(1).HasValue() && p.peek(1).MustGetValue().tokenType == equals {
-
+		return opt.ToOptional(NodeStmt{node})
+	} else if tok := p.tryConsume(identifier); tok.HasValue() {
 		node := NodeStmtVarAssign{
-			ident: p.consume(),
+			ident: tok.MustGetValue(),
 		}
-		p.consume()
+		p.mustTryConsume(equals, "expected '=' after variable name for assignment")
 
 		if expr := p.ParseExpr(); expr.HasValue() {
 			node.expr = expr.MustGetValue()
@@ -101,25 +73,20 @@ func (p *Parser) ParseStmt() opt.Optional[NodeStmt] {
 			panic(errors.New("invalid expression"))
 		}
 
-		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == semiColon {
-			p.consume()
-		} else {
-			panic(errors.New("missing ';'"))
-		}
+		p.mustTryConsume(semiColon, "missing ';'")
 
-		return opt.ToOptional(NodeStmt{variant: node})
+		return opt.ToOptional(NodeStmt{node})
 	} else {
 		return opt.Optional[NodeStmt]{}
 	}
 }
 
 func (p *Parser) ParseTerm() opt.Optional[NodeTerm] {
-	if p.peek().HasValue() && p.peek().MustGetValue().tokenType == intLiteral {
-		return opt.ToOptional(NodeTerm{variant: NodeTermIntLiteral{intLiteral: p.consume()}})
-
-	} else if p.peek().HasValue() && p.peek().MustGetValue().tokenType == identifier {
-		return opt.ToOptional(NodeTerm{variant: NodeTermIdentifier{identifier: p.consume()}})
-
+	if tok := p.tryConsume(intLiteral); tok.HasValue() {
+		return opt.ToOptional(NodeTerm{NodeTermIntLiteral{tok.MustGetValue()}})
+	}
+	if tok := p.tryConsume(identifier); tok.HasValue() {
+		return opt.ToOptional(NodeTerm{NodeTermIdentifier{tok.MustGetValue()}})
 	}
 	return opt.Optional[NodeTerm]{}
 }
@@ -127,25 +94,24 @@ func (p *Parser) ParseTerm() opt.Optional[NodeTerm] {
 func (p *Parser) ParseExpr() opt.Optional[NodeExpr] {
 	if term := p.ParseTerm(); term.HasValue() {
 		if p.peek().HasValue() && p.peek().MustGetValue().tokenType == plus {
-			return opt.ToOptional(NodeExpr{variant: p.ParseBinExpr(term.MustGetValue()).MustGetValue()})
+			return opt.ToOptional(NodeExpr{p.ParseBinExpr(term.MustGetValue()).MustGetValue()})
 		} else {
-			return opt.ToOptional(NodeExpr{variant: term.MustGetValue()})
+			return opt.ToOptional(NodeExpr{term.MustGetValue()})
 		}
 	}
 	return opt.Optional[NodeExpr]{}
 }
 
 func (p *Parser) ParseBinExpr(lhs NodeTerm) opt.Optional[NodeBinExpr] {
-	if p.peek().HasValue() && p.peek().MustGetValue().tokenType == plus {
+	if p.tryConsume(plus).HasValue() {
 		node := NodeBinExprAdd{}
-		node.left = NodeExpr{variant: lhs}
-		p.consume()
+		node.left = NodeExpr{lhs}
 		if rhs := p.ParseExpr(); rhs.HasValue() {
 			node.right = rhs.MustGetValue()
 		} else {
 			panic(errors.New("expected expression"))
 		}
-		return opt.ToOptional(NodeBinExpr{variant: node})
+		return opt.ToOptional(NodeBinExpr{node})
 	} else {
 		panic(errors.New("unsupported binary operator"))
 	}
@@ -169,6 +135,21 @@ func (p *Parser) consume() Token {
 	r := p.tokens[p.currentIndex]
 	p.currentIndex++
 	return r
+}
+
+func (p *Parser) mustTryConsume(tokType TokenType, errMsg string) Token {
+	if p.peek().HasValue() && p.peek().MustGetValue().tokenType == tokType {
+		return p.consume()
+	} else {
+		panic(errors.New(errMsg))
+	}
+}
+func (p *Parser) tryConsume(tokType TokenType) opt.Optional[Token] {
+	if p.peek().HasValue() && p.peek().MustGetValue().tokenType == tokType {
+		return opt.ToOptional(p.consume())
+	} else {
+		return opt.Optional[Token]{}
+	}
 }
 
 type NodeProg struct {
