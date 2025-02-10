76af8100332a05fe4dc9c5655f90aa263cdc8fda
MoltenWolfCub <oliver@tomlins.org.uk>
2025-02-10 23:11:43 +0000

    wrote an exit function using syscall

--- build/testSyscall.asm
+++ build/testSyscall.asm
@@ -1,20 +1,38 @@
 global _start
 
 
-_start:
+_exit_1:
+	;=====FUNCTION SETUP=====
+	push rbp
+	mov rbp, rsp
+
+	;=====FUNCTION BODY=====
 	mov rax, 60
 	push rax
+	push QWORD [rbp + 16]
+	pop rdi
+	pop rax
+	syscall
+
+	add rsp, 0
+	;=====FUNCTION CLEANUP=====
+	pop rbp
+	ret
+
+
+_start:
+
 	mov rax, 3
 	push rax
-	mov rax, 2
+	mov rax, 8
 	push rax
 	pop rbx
 	pop rax
 	add rax, rbx
 	push rax
-	pop rdi
-	pop rax
-	syscall
+	call _exit_1
+	add rsp, 8
+	add rsp, 0
 
 	mov rax, 60
 	mov rdi, 0
